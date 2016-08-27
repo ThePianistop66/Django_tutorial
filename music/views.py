29ff34796412dcda8b94b1d03ddcc5e8f1be5918
ThePianistop66 <kassis.andrew@gmail.com>
2016-08-27 13:29:40 -0400

    Added radio form for favorites

--- music/views.py
+++ music/views.py
@@ -20,15 +20,20 @@ def detail(request, album_id):
 
 def favorite(request, album_id):
     album = get_object_or_404(Album, id=album_id)
+
     try:
         selected_song = album.song_set.get(id=request.POST['song'])
     except (KeyError, Song.DoesNotExist):
         return render(request, 'music/detail.html', {
             'album': album,
             'error_message':'You did not select a valid song!'
-        })
+    })
     else:
-        selected_song.is_favorite=True
-        selected_song.save()
-        return HttpResponseRedirect(reverse('music:detail', args=album_id))
+        if selected_song.is_favorite == True:
+            selected_song.is_favorite = False
+            selected_song.save()
+        else:
+            selected_song.is_favorite=True
+            selected_song.save()
+    return HttpResponseRedirect(reverse('music:detail', args=album_id))
 
